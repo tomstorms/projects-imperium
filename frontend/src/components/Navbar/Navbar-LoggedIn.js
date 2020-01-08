d939a2bc135bcdc4723e604c41011bd0677d49a3
Tom Storms <tom@orangedigital.com.au>
2020-01-08 20:57:36 +1000

    Remove unused code

--- frontend/src/components/Navbar/Navbar-LoggedIn.js
+++ frontend/src/components/Navbar/Navbar-LoggedIn.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 
 import Select from '../../components/Select/Select';
-import Icon from '../../components/Icon/Icon';
 
 import './Navbar.css';
 
