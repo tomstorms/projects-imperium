7e5ef8017ed766c2f13571e14b6342c13b19e462
Tom Storms <tom@orangedigital.com.au>
2020-01-08 20:57:53 +1000

    Remove unwanted code

--- frontend/src/pages/Contacts/Contacts.js
+++ frontend/src/pages/Contacts/Contacts.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { NavLink } from 'react-router-dom';
 
 import AuthContext from '../../context/auth-context';
 import Spinner from '../../components/Spinner/Spinner';
