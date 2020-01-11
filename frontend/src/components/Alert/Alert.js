77dc525e5e0f0ab6ad3cd5d4280795844e642d21
Tom Storms <tom@orangedigital.com.au>
2020-01-11 17:06:40 +1000

    Fix Alert component styling

--- frontend/src/components/Alert/Alert.js
+++ frontend/src/components/Alert/Alert.js
@@ -4,7 +4,7 @@ import './Alert.css';
 
 const alert = props => (
     <React.Fragment>
-        <div className={`alert alert--${props.type}`}>
+        <div className={`alert${props.type ? ' alert--' + props.type : ''}`}>
             <div className="message">{props.message}</div>
         </div>
     </React.Fragment>
